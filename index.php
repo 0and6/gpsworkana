2e604431e34cc6fbd0d497760bf80128c24bf070
0and6 <0and6@outlook.com>
2020-10-29 22:10:12 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,11 @@ if(isset($_GET['lat']) && isset($_GET['lon'])) {
     echo "Ok<br>";
     echo "lat:".$_GET['lat'] . "   lon:" . $_GET['lon'] . "<br>";
     echo "Repeat35s";
+    
+    $conexion = pg_connect("host=ec2-34-234-185-150.compute-1.amazonaws.com dbname=d212b7tb07aa7e user=iqxlvrecdwtpye password=0a1efa1914fdc940758ab64d009da5af3be3c8f863e5d9ad7c9674278479d2d4");
+
+    pg_insert($conexion, 'coordenadas', $_GET);
+    
 } else {
     echo "Error";
 }
