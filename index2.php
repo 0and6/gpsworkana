55eb804d0f4f03e1d8a9a2aaeb3c6031addc9f11
0and6 <0and6@outlook.com>
2020-11-10 18:34:27 -0600

    Agregar soporte para envios en POST

---
+++ index2.php
@@ -0,0 +1,23 @@
+<?php
+if(isset($_POST['lat']) && isset($_POST['lon'])) {
+    echo "Ok<br>";
+    echo "lat:".$_POST['lat'] . "   lon:" . $_POST['lon'] . "<br>";
+    echo "Repeat 5s";
+
+    $conexion = pg_connect("host=ec2-34-234-185-150.compute-1.amazonaws.com dbname=d212b7tb07aa7e user=iqxlvrecdwtpye password=0a1efa1914fdc940758ab64d009da5af3be3c8f863e5d9ad7c9674278479d2d4");
+    echo "se realizo la conexion <br>";
+
+    $datos = array('latitud' => $_POST['lat'], 'longitud' => $_POST['lon'], 'fecha' => date_format(new DateTime("now"),"Y-m-d H:i:s"));
+    print_r($datos);
+    $insertar = pg_insert($conexion, 'coordenadas', $datos);
+    if($insertar) {
+        echo "datos insertados correctamente";
+    } else {
+        echo "fallo al insertar";
+    }
+
+
+} else {
+    echo "Error";
+}
+?>
---
+++ send2.php
@@ -0,0 +1,7 @@
+<form action="index2.php" method="POST">
+  <label for="lat">Latitud: </label>
+  <input type="text" id="lat" name="lat"><br><br>
+  <label for="lon">Longitud:</label>
+  <input type="text" id="lon" name="lon"><br><br>
+  <input type="submit" value="Submit">
+</form> 
